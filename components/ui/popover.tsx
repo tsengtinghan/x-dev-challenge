b4e9b7b73d9e7cccce235ecb48596e1604ad6d05
Marina Levay <63079525+marilevay@users.noreply.github.com>
2024-04-21 01:42:27 -0700

    new commits

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -6,49 +6,28 @@ import * as PopoverPrimitive from "@radix-ui/react-popover"
 import { cn } from "@/lib/utils"
 
 const Popover = PopoverPrimitive.Root
+
 const PopoverTrigger = PopoverPrimitive.Trigger
 
 const PopoverContent = React.forwardRef<
-  HTMLDivElement,
+  React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(
-  ({ className = '', ...props }, ref) => {
-    const [position, setPosition] = React.useState({ top: 0, left: 0 })
-
-    const calculatePosition = () => {
-      const windowWidth = window.innerWidth
-      const windowHeight = window.innerHeight
-      const popoverWidth = 200 // Adjust this according to your popover width
-      const popoverHeight = 200 // Adjust this according to your popover height
-
-      const left = (windowWidth - popoverWidth) / 2
-      const top = (windowHeight - popoverHeight) / 2
-
-      setPosition({ left, top })
-    }
-
-    React.useEffect(() => {
-      calculatePosition()
-      window.addEventListener("resize", calculatePosition)
-      return () => window.removeEventListener("resize", calculatePosition)
-    }, [])
-
-    return (
-      <PopoverPrimitive.Portal>
-        <PopoverPrimitive.Content
-          ref={ref}
-          style={{ top: position.top, left: position.left }}
-          className={cn(
-            "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-            className
-          )}
-          {...props}
-        />
-      </PopoverPrimitive.Portal>
-    )
-  }
-)
+>(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+  <PopoverPrimitive.Portal>
+    <PopoverPrimitive.Content
+      ref={ref}
+      align={align}
+      sideOffset={sideOffset}
+      className={cn(
+        "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        className
+      )}
+      {...props}
+    />
+  </PopoverPrimitive.Portal>
+))
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
-export { Popover, PopoverTrigger, PopoverContent }
+const PopoverClose = PopoverPrimitive.Close
 
+export { Popover, PopoverTrigger, PopoverContent, PopoverClose }
--- components/ui/popup-create.tsx
+++ components/ui/popup-create.tsx
@@ -15,39 +15,84 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
+  PopoverClose,
 } from "@/components/ui/popover";
 
-const submitImport = () => {
-  console.log("Submit import");
-};
+const API_BASE_URL = "https://xlearn-rnuz.onrender.com";
 
-export function PopoverDemo() {
+function submitData(endpoint: string, data: {}) {
+  console.log(`${API_BASE_URL}/${endpoint}`, JSON.stringify(data));
+  return fetch(`${API_BASE_URL}/${endpoint}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((e) => {
+      console.error("Error submitting data:", e);
+    });
+}
+
+export function PopoverDemo({ onAddMaterial, user_id } : { onAddMaterial: () => void , user_id: string }) {
   const [formData, setFormData] = useState({
     importUrl: "",
     customPrompt: "",
     quote: "",
     question: "",
-    answer: ""
+    answer: "",
   });
 
-  const submitImport = () => {
-    console.log("Submit import");
+  const handleSubmit = async (
+    event: React.MouseEvent<HTMLButtonElement>,
+    formType: string
+  ) => {
+    // event.preventDefault();
+    let endpoint = "";
+    let data = {};
+
+    switch (formType) {
+      case "import":
+        endpoint = "import";
+        data = { url: formData.importUrl, prompt: formData.customPrompt };
+        break;
+      case "quote":
+        endpoint = "quotes";
+        data = { quote: formData.quote };
+        break;
+      case "question":
+        endpoint = "question";
+        data = { user_id: user_id, question: formData.question, answer: formData.answer };
+        console.log(data);
+        break;
+      default:
+        return;
+    }
+    console.log("Submitting data:", data);
+    const response = await submitData(endpoint, data);
+    console.log("Server response:", response);
+    onAddMaterial();
   };
-  
-  const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button>New</Button>
+        <Button type='button'>New</Button>
       </PopoverTrigger>
-      <PopoverContent className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <PopoverContent className="w-[auto]">
         <Tabs defaultValue="import" className="w-[600px]">
           <TabsList className="grid w-full grid-cols-3">
             <TabsTrigger value="import">Import</TabsTrigger>
@@ -65,15 +110,29 @@ export function PopoverDemo() {
               <CardContent className="space-y-2">
                 <div className="space-y-1">
                   <Label htmlFor="url">URL</Label>
-                  <Input id="url" name="importUrl" value={formData.importUrl} onChange={handleChange} />
+                  <Input
+                    id="url"
+                    name="importUrl"
+                    value={formData.importUrl}
+                    onChange={handleChange}
+                  />
                 </div>
                 <div className="space-y-1">
                   <Label htmlFor="customPrompt">Custom Prompt</Label>
-                  <Input id="customPrompt" name="customPrompt" value={formData.customPrompt} onChange={handleChange} />
+                  <Input
+                    id="customPrompt"
+                    name="customPrompt"
+                    value={formData.customPrompt}
+                    onChange={handleChange}
+                  />
                 </div>
               </CardContent>
               <CardFooter>
-                <Button onClick={submitImport}>Add</Button>
+                <PopoverClose>
+                  <Button onClick={(e) => handleSubmit(e, "import")}>
+                    Add
+                  </Button>
+                </PopoverClose>
               </CardFooter>
             </Card>
           </TabsContent>
@@ -82,17 +141,25 @@ export function PopoverDemo() {
               <CardHeader>
                 <CardTitle>Quotes</CardTitle>
                 <CardDescription>
-                  Change your password here. After saving, youll be logged out.
+                  Change your password here. After saving, you'll be logged out.
                 </CardDescription>
               </CardHeader>
               <CardContent className="space-y-2">
                 <div className="space-y-1">
                   <Label htmlFor="quote">Quote</Label>
-                  <Input id="quote" name="quote" type="text" value={formData.quote} onChange={handleChange} />
+                  <Input
+                    id="quote"
+                    name="quote"
+                    type="text"
+                    value={formData.quote}
+                    onChange={handleChange}
+                  />
                 </div>
               </CardContent>
               <CardFooter>
-                <Button>Add</Button>
+                <PopoverClose>
+                  <Button onClick={(e) => handleSubmit(e, "quote")}>Add</Button>
+                </PopoverClose>
               </CardFooter>
             </Card>
           </TabsContent>
@@ -101,21 +168,35 @@ export function PopoverDemo() {
               <CardHeader>
                 <CardTitle>Questions</CardTitle>
                 <CardDescription>
-                  Make changes to your Import here. Click save when youre done.
+                  Make changes to your Import here. Click save when you're done.
                 </CardDescription>
               </CardHeader>
               <CardContent className="space-y-2">
                 <div className="space-y-1">
                   <Label htmlFor="question">Question</Label>
-                  <Input id="question" name="question" value={formData.question} onChange={handleChange} />
+                  <Input
+                    id="question"
+                    name="question"
+                    value={formData.question}
+                    onChange={handleChange}
+                  />
                 </div>
                 <div className="space-y-1">
                   <Label htmlFor="answer">Answer</Label>
-                  <Input id="answer" name="answer" value={formData.answer} onChange={handleChange} />
+                  <Input
+                    id="answer"
+                    name="answer"
+                    value={formData.answer}
+                    onChange={handleChange}
+                  />
                 </div>
               </CardContent>
               <CardFooter>
-                <Button>Add</Button>
+                <PopoverClose asChild>
+                  <Button onClick={(e) => handleSubmit(e, "question")}>
+                    Add
+                  </Button>
+                </PopoverClose>
               </CardFooter>
             </Card>
           </TabsContent>
